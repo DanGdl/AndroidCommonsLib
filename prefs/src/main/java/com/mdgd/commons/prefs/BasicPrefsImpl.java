d24e97d28abb8d82ce33312f74f0759e4e0434e4
Dan <danglad@mutham.co.il>
2018-09-05 11:22:37 +0300

    # prefs fix

--- prefs/src/main/java/com/mdgd/commons/prefs/BasicPrefsImpl.java
+++ prefs/src/main/java/com/mdgd/commons/prefs/BasicPrefsImpl.java
@@ -38,119 +38,119 @@ abstract class BasicPrefsImpl {
 
 
 
-    protected String get(String fileName, String key, String defaultVal) {
+    public String get(String fileName, String key, String defaultVal) {
         return getPrefs(fileName).getString(key, defaultVal);
     }
 
-    protected String get(String key, String defaultVal){
+    public String get(String key, String defaultVal){
         return getPrefs(getDefaultPrefsFileName()).getString(key, defaultVal);
     }
 
-    protected void put(String fileName, String key, String value) {
+    public void put(String fileName, String key, String value) {
         getEditor(fileName).putString(key, value).apply();
     }
 
-    protected void put(String key, String value){
+    public void put(String key, String value){
         getEditor(getDefaultPrefsFileName()).putString(key, value).apply();
     }
 
 
 
-    protected int get(String fileName, String key, int defaultVal) {
+    public int get(String fileName, String key, int defaultVal) {
         return getPrefs(fileName).getInt(key, defaultVal);
     }
 
-    protected int get(String key, int defaultVal){
+    public int get(String key, int defaultVal){
         return getPrefs(getDefaultPrefsFileName()).getInt(key, defaultVal);
     }
 
-    protected void put(String fileName, String key, int value) {
+    public void put(String fileName, String key, int value) {
         getEditor(fileName).putInt(key, value).apply();
     }
 
-    protected void put(String key, int value){
+    public void put(String key, int value){
         getEditor(getDefaultPrefsFileName()).putInt(key, value).apply();
     }
 
 
 
-    protected boolean get(String fileName, String key, boolean defaultVal) {
+    public boolean get(String fileName, String key, boolean defaultVal) {
         return getPrefs(fileName).getBoolean(key, defaultVal);
     }
 
-    protected boolean get(String key, boolean defaultVal){
+    public boolean get(String key, boolean defaultVal){
         return getPrefs(getDefaultPrefsFileName()).getBoolean(key, defaultVal);
     }
 
-    protected void put(String fileName, String key, boolean value) {
+    public void put(String fileName, String key, boolean value) {
         getEditor(fileName).putBoolean(key, value).apply();
     }
 
-    protected void put(String key, boolean value){
+    public void put(String key, boolean value){
         getEditor(getDefaultPrefsFileName()).putBoolean(key, value).apply();
     }
 
 
 
-    protected long get(String fileName, String key, long defaultVal) {
+    public long get(String fileName, String key, long defaultVal) {
         return getPrefs(fileName).getLong(key, defaultVal);
     }
 
-    protected long get(String key, long defaultVal){
+    public long get(String key, long defaultVal){
         return getPrefs(getDefaultPrefsFileName()).getLong(key, defaultVal);
     }
 
-    protected void put(String fileName, String key, long value) {
+    public void put(String fileName, String key, long value) {
         getEditor(fileName).putLong(key, value).apply();
     }
 
-    protected void put(String key, long value){
+    public void put(String key, long value){
         getEditor(getDefaultPrefsFileName()).putLong(key, value).apply();
     }
 
 
 
-    protected float get(String fileName, String key, float defaultVal) {
+    public float get(String fileName, String key, float defaultVal) {
         return getPrefs(fileName).getFloat(key, defaultVal);
     }
 
-    protected float get(String key, float defaultVal){
+    public float get(String key, float defaultVal){
         return getPrefs(getDefaultPrefsFileName()).getFloat(key, defaultVal);
     }
 
-    protected void put(String fileName, String key, float value) {
+    public void put(String fileName, String key, float value) {
         getEditor(fileName).putFloat(key, value).apply();
     }
 
-    protected void put(String key, float value){
+    public void put(String key, float value){
         getEditor(getDefaultPrefsFileName()).putFloat(key, value).apply();
     }
 
 
 
-    protected Set<String> get(String fileName, String key, Set<String> defaultVal) {
+    public Set<String> get(String fileName, String key, Set<String> defaultVal) {
         return getPrefs(fileName).getStringSet(key, defaultVal);
     }
 
-    protected Set<String> get(String key, Set<String> defaultVal){
+    public Set<String> get(String key, Set<String> defaultVal){
         return getPrefs(getDefaultPrefsFileName()).getStringSet(key, defaultVal);
     }
 
-    protected void put(String fileName, String key, Set<String> value) {
+    public void put(String fileName, String key, Set<String> value) {
         getEditor(fileName).putStringSet(key, value).apply();
     }
 
-    protected void put(String key, Set<String> value){
+    public void put(String key, Set<String> value){
         getEditor(getDefaultPrefsFileName()).putStringSet(key, value).apply();
     }
 
 
 
-    protected Map<String, ?> get(String fileName) {
+    public Map<String, ?> get(String fileName) {
         return getPrefs(fileName).getAll();
     }
 
-    protected Map<String, ?> get(){
+    public Map<String, ?> get(){
         return getPrefs(getDefaultPrefsFileName()).getAll();
     }
 }

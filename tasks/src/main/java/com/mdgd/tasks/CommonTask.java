cf05ed1df6f02719225c9da01e0f1d1f1c2ebdd6
Max <maxmmelnikov@gmail.com>
2019-02-01 22:15:07 +0200

    # cursor parser fix # foreground delegate # libs update

--- tasks/src/main/java/com/mdgd/tasks/CommonTask.java
+++ tasks/src/main/java/com/mdgd/tasks/CommonTask.java
@@ -7,6 +7,7 @@ import com.mdgd.commons.contract.services.ServiceContract;
  * Created by Max
  * on 13/06/2018.
  */
+@Deprecated
 public abstract class CommonTask<T extends ServiceContract.IPresenter> extends GcmTaskService  implements ServiceContract.IService {
 
     protected final T presenter;

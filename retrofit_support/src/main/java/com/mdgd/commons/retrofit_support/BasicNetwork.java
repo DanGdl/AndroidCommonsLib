f8a7f8d4a9d2fe3a694be9dc7bb85856b9f32abf
Dan <danglad@mutham.co.il>
2019-02-20 10:29:46 +0200

    # retrofit support new methods

--- retrofit_support/src/main/java/com/mdgd/commons/retrofit_support/BasicNetwork.java
+++ retrofit_support/src/main/java/com/mdgd/commons/retrofit_support/BasicNetwork.java
@@ -9,8 +9,17 @@ import retrofit2.Call;
  * on 05/02/2019.
  */
 public class BasicNetwork {
+
+    protected <T, X> void execAsync(Call<T> call, ICallback<X> callback) {
+        execAsync(call, callback, null);
+    }
+
     protected <T, X> void execAsync(Call<T> call, ICallback<X> callback, ITransformer<T, X> transform) {
         call.enqueue(new RetroCallbackImpl<>(callback, transform));
+    }
+
+    protected <T, X> void execSync(Call<T> call, ICallback<X> callback) {
+        execSync(call, callback, null);
     }
 
     protected <T, X> void execSync(Call<T> call, ICallback<X> callback, ITransformer<T, X> transform) {
